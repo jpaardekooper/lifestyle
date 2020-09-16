0a6bc2aee396ac8564e0c27e66c816661401a914
Jasper <jasper.paardekooper@outlook.com>
2020-09-16 09:28:24 +0200

    added error handling

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -12,38 +12,86 @@ class AuthService {
 
   // Example code of how to sign in with email and password.
   Future signInWithEmailAndPassword(String email, String password) async {
+    User user;
+    String errorMessage;
+
     try {
-      final User user = (await _auth.signInWithEmailAndPassword(
+      user = (await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       ))
           .user;
-      print("scuccesvol");
-      return user;
-    } catch (e) {
-      print("niet scuccesvol");
-      return null;
+    } catch (error) {
+      switch (error.code) {
+        case "ERROR_INVALID_EMAIL":
+          errorMessage = "Your email address appears to be malformed.";
+          break;
+        case "ERROR_WRONG_PASSWORD":
+          errorMessage = "Your password is wrong.";
+          break;
+        case "ERROR_USER_NOT_FOUND":
+          errorMessage = "User with this email doesn't exist.";
+          break;
+        case "ERROR_USER_DISABLED":
+          errorMessage = "User with this email has been disabled.";
+          break;
+        case "ERROR_TOO_MANY_REQUESTS":
+          errorMessage = "Too many requests. Try again later.";
+          break;
+        case "ERROR_OPERATION_NOT_ALLOWED":
+          errorMessage = "Signing in with Email and Password is not enabled.";
+          break;
+        default:
+          errorMessage = "An undefined Error happened.";
+      }
     }
+
+    if (errorMessage != null) {
+      return Future.error(errorMessage);
+    }
+
+    return user.uid;
   }
 
   Future signUpWithEmailAndPassword(String email, String password) async {
+    User user;
+    String errorMessage;
+
     try {
-      final User user = (await _auth.createUserWithEmailAndPassword(
+      user = (await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       ))
           .user;
-      if (user != null) {
-        return user;
-      } else {
-        return null;
-      }
-    } catch (signUpError) {
-      if (signUpError is PlatformException) {
-        if (signUpError.code == 'ERROR_EMAIL_ALREADY_IN_USE') {
-          return "email al in gebruik";
-        }
+    } catch (error) {
+      switch (error.code) {
+        case "ERROR_INVALID_EMAIL":
+          errorMessage = "Your email address appears to be malformed.";
+          break;
+        case "ERROR_WRONG_PASSWORD":
+          errorMessage = "Your password is wrong.";
+          break;
+        case "ERROR_USER_NOT_FOUND":
+          errorMessage = "User with this email doesn't exist.";
+          break;
+        case "ERROR_USER_DISABLED":
+          errorMessage = "User with this email has been disabled.";
+          break;
+        case "ERROR_TOO_MANY_REQUESTS":
+          errorMessage = "Too many requests. Try again later.";
+          break;
+        case "ERROR_OPERATION_NOT_ALLOWED":
+          errorMessage = "Signing in with Email and Password is not enabled.";
+          break;
+        default:
+          errorMessage = "An undefined Error happened.";
       }
     }
+
+    if (errorMessage != null) {
+      return Future.error(errorMessage);
+    }
+
+    return user.uid;
   }
 }
